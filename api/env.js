3ab0c040addc6dd4def5b85579c2098e0b811fbc
lishengguo <lishengguo@qq.com>
2017-08-11 19:08:24 +0800

    支持传入参数、层级

--- api/env.js
+++ api/env.js
@@ -1,22 +1,37 @@
-function translate(cfg, level = 1) {
-  for (var key in cfg) {
+var env = {
+
+}
+function translate(cfg, valueObj = getENV(), level = 1) {
+  Object.keys(cfg).forEach(key => {
     var value = cfg[key];
-    if (!value || !cfg.hasOwnProperty(key)) continue;
-    if (typeof value == "object" && level > 1) {
-      level--;
-      translate(value)
+    if (value && typeof value == "object" && level > 1) {
+      translate(value, valueObj, --level)
     }
     else if (typeof value == "string") {
       var match = value.match(/\${(\w+)}/g);
       match && match.forEach(m => {
         var word = m.match(/\w+/g)[0];
-        var evnValue = process.env[word];
+        var evnValue = valueObj[word];
         value = value.replace(new RegExp("\\$\\{" + word + "\\}", "g"), evnValue);
       })
       cfg[key] = value;
     }
-  }
+  })
+
   return cfg;
+}
+
+function getENV() {
+  if (!env._inited) {
+    env._inited = true
+    Object.assign(env, process.env)
+    process.argv.filter(i => i.indexOf("-D") == 0).forEach(k => {
+      var key = k.split("=")[0].substr(2).trim()
+      var value = k.substr(k.indexOf("=") + 1)
+      env[key] = value
+    })
+  }
+  return env
 }
 
 module.exports = translate;
